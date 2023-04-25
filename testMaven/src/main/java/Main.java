f8a23b8082fac414e9837429f0ec29f0e3e536d4
alvib <81896138+alvib@users.noreply.github.com>
2023-04-25 20:56:16 +0400

    Update Main.java

--- testMaven/src/main/java/Main.java
+++ testMaven/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    Human human = new Human("John", "Adler");
-    System.out.println(human.getFirstName() + " " + human.getLastName());
+        Human human = new Human("John", "Adler");
+        System.out.println(human);
     }
 }
